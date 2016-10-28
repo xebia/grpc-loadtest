0a7048edac99a942f5905147fa6973d96e37d0c7
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2016-10-28 11:46:28 +0200

    server

---
+++ src/python/multiplex_server.py
@@ -0,0 +1,40 @@
+from concurrent import futures
+import time
+import math
+import random
+import grpc
+import device_pb2
+
+
+class TemperatureService(device_pb2.TemperatureServiceServicer):
+    def set_temperature(self, request, context):
+	print request
+
+    def get_temperature(self, request, context):
+	yield 20.0 + random.random()
+
+class UsageService(device_pb2.UsageServiceServicer):
+  def latest_electricity_usage(self, request, context):
+    print request
+
+  def latest_gas_usage(self, request, context):
+    print request
+
+
+
+def serve():
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+  device_pb2.add_TemperatureServiceServicer_to_server(TemperatureService(), server)
+  device_pb2.add_UsageServiceServicer_to_server(UsageService(), server)
+  server.add_insecure_port('[::]:50051')
+  server.start()
+  print 'server running on port %s' % '50051'
+  try:
+    while True:
+      time.sleep(24 * 2600)
+  except KeyboardInterrupt:
+    server.stop(0)
+
+
+if __name__ == '__main__':
+  serve()
