0e28194f89ef160d028b969f81f09a6d3fdc03b8
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2016-10-28 15:37:16 +0200

    now using device uuid

--- src/node/client.js
+++ src/node/client.js
@@ -1,20 +1,22 @@
 var PROTO_PATH = __dirname + '/../proto/device.proto';
 
 var grpc = require('grpc');
+var uuid = require('node-uuid');
 
 var device_proto = grpc.load(PROTO_PATH).device;
 
 function main() {
+  var device = uuid.v1();
   var client = new device_proto.TemperatureService('localhost:50051',
                                        grpc.credentials.createInsecure())
 
-  client.setTemperature({celcius: 22.0 }, function(err, response) {
+  client.setTemperature({device: device, celcius: 22.0 }, function(err, response) {
     if(err) {
        console.log('set temperature failed', err);
     }
   });
 
-  call = client.getTemperature({});
+  call = client.getTemperature({device: device});
   call.on('data', function(temperature) {
 	console.log(temperature);
   });
