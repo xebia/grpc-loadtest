f4cd6556c61ac61b72c2fcb758115f812fb5c1bb
Adriaan Thomas <athomas@xebia.com>
2016-10-29 20:12:24 +0200

    Promisify all remote calls

--- src/node/client.js
+++ src/node/client.js
@@ -2,24 +2,29 @@
 
 const grpc = require('grpc');
 const uuid = require('node-uuid');
+const Promise = require('bluebird');
 
 const PROTO_PATH = __dirname + '/../proto/device.proto';
 const device_proto = grpc.load(PROTO_PATH).device;
 
 function main() {
   const device = uuid.v1();
-  const client = new device_proto.TemperatureService('localhost:50051', grpc.credentials.createInsecure())
+  const client = Promise.promisifyAll(
+    new device_proto.TemperatureService('localhost:50051', grpc.credentials.createInsecure())
+  );
 
-  client.setTemperature({ device: device, celcius: 22.0 }, (err, response) => {
-    if (err) {
-      console.log('set temperature failed', err);
-    }
-
-    const call = client.getTemperature({ device: device });
-    call.on('data', temperature => console.log(temperature));
-    call.on('end', () =>  console.log('end'));
-    call.on('error', console.error);
-  });
+  client.setTemperatureAsync({ device, celcius: 22.0 })
+    .catch(e => console.error('set temperature failed', e))
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        const call = client.getTemperature({ device });
+        call.on('data', console.log);
+        call.on('end', resolve);
+        call.on('error', reject);
+      });
+    })
+    .then(() => console.log('end'))
+    .catch(e => console.error('get temperature failed', e));
 }
 
 main();
